439e7743637b40c84b2ec666bc77bb36761f7203
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-10-24 13:46:13 -0600

    update

--- src/Projects.js
+++ src/Projects.js
@@ -31,6 +31,44 @@ const Projects = () => {
             <div className="projects">
                 <div className="project1" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
                     <div className='middle'>
+                        <a href="http://crypto-dash.surge.sh/" target="_blank" rel="noopener noreferrer">
+                            <div className='projectInfo' style={{marginTop:"-1.5em"}}>
+                                <h1 className="nav" style={{color:"#ff9450", padding:"2%"}}>Crypto Dash</h1>
+                                <h4>
+                                    Simple app to help users quickly track cryptocurrency stocks live whilst keeping up to date with the latest crypto news.
+                                </h4>
+                                <p style={{color:"lime", padding:"5%", margin: 0}}>React, JavaScript, MongoDB, Mongoose, Axios, Node.js, JSX, HTML, & CSS</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="project2" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
+                    <div className='middle'>
+                        <a href="http://cuisine-hunter.surge.sh/" target="_blank" rel="noopener noreferrer">
+                            <div className='projectInfo' style={{marginTop:"-1em"}}>
+                                <h1 className="nav" style={{color:"#ff9450", padding:"2%"}}>Cuisine Hunter</h1>
+                                <h4>An app created with React.js to help users find restuarants in Utah serving desired cuisines.
+                                </h4>
+                                <p style={{color:"lime", padding:"5%", margin: 0}}>JavaScript, React, Axios, HTML, CSS, & JSX</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="project3" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
+                    <div className='middle'>
+                        <a href="http://30-apps-challenge.surge.sh/" target="_blank" rel="noopener noreferrer">
+                            <div className='projectInfo' style={{marginTop:"-1.5em"}}>
+                                <h1 className="nav" style={{color:"#ff9450", padding:"2%"}}>30 React Apps</h1>
+                                <h4>
+                                    A challenge to create 30 apps in 30 days. Inspired by Jennifer Dewalt.
+                                </h4>
+                                <p style={{color:"lime", padding:"5%", margin: 0}}>React, JavaScript, multiple third party API's, Axios, Node.js, JSX, HTML, & CSS</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div className="project4" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
+                    <div className='middle'>
                         <a href="http://javascript30-home.surge.sh/" target="_blank" rel="noopener noreferrer">
                             <div className='projectInfo' style={{marginTop:"-1.5em"}}>
                                 <h1 className="nav" style={{color:"#ff9450", padding:"2%"}}>
@@ -46,44 +84,9 @@ const Projects = () => {
                         </a>
                     </div>
                 </div>
-                <div className="project2" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
-                    <div className='middle'>
-                        <a href="http://30-apps-challenge.surge.sh/" target="_blank" rel="noopener noreferrer">
-                            <div className='projectInfo' style={{marginTop:"-1.5em"}}>
-                                <h1 className="nav" style={{color:"#ff9450", padding:"2%"}}>React 30 in 30</h1>
-                                <h4>
-                                    A challenge to create 30 apps in 30 days. Inspired by Jennifer Dewalt.
-                                </h4>
-                                <p style={{color:"lime", padding:"5%", margin: 0}}>React, JavaScript, multiple third party API's, Axios, Node.js, JSX, HTML, & CSS</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div className="project3" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
-                    <div className='middle'>
-                        <a href="http://crypto-dash.surge.sh/" target="_blank" rel="noopener noreferrer">
-                            <div className='projectInfo' style={{marginTop:"-1.5em"}}>
-                                <h1 className="nav" style={{color:"#ff9450", padding:"2%"}}>Crypto Dash</h1>
-                                <h4>
-                                    Simple app to help users quickly track cryptocurrency stocks live whilst keeping up to date with the latest crypto news.
-                                </h4>
-                                <p style={{color:"lime", padding:"5%", margin: 0}}>React, JavaScript, MongoDB, Mongoose, Axios, Node.js, JSX, HTML, & CSS</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div className="project4" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
-                    <div className='middle'>
-                        <a href="http://cuisine-hunter.surge.sh/" target="_blank" rel="noopener noreferrer">
-                            <div className='projectInfo' style={{marginTop:"-1em"}}>
-                                <h1 className="nav" style={{color:"#ff9450", padding:"2%"}}>Cuisine Hunter</h1>
-                                <h4>An app created with React.js to help users find restuarants in Utah serving desired cuisines.
-                                </h4>
-                                <p style={{color:"lime", padding:"5%", margin: 0}}>JavaScript, React, Axios, HTML, CSS, & JSX</p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+
+
+
                 <div className="project5" style={{ cursor:"pointer", backgroundSize: "cover", backgroundRepeat:"no-repeat", height: "vh"}}>
                     <div className='middle'>
                         <a href="http://cuisine-hunter.surge.sh/" target="_blank" rel="noopener noreferrer">
